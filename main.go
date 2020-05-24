c38e3f96ddc9ffe04252ddd896dce68fc182f860
Connor <fox@nerdfox.me>
2020-05-24 01:11:49 -0600

    Mongodb port

--- main.go
+++ main.go
@@ -38,7 +38,7 @@ func main() {
 	//Sets up the connection to the database
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
-	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://127.0.0.1"))
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb+srv://localhost:27017"))
 	if err != nil {
 		panic(err)
 	}
